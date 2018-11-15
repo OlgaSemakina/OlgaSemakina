53f31dd7626ea7f71e6e6ff6cf708b5200f4cb51
OlgaSemakina <semakina.olga@mail.ru>
2018-11-15 21:59:39 +0300

    Homework 6 fix2

--- src/main/java/runners/CucumberTestngRunnerHw6.java
+++ src/main/java/runners/CucumberTestngRunnerHw6.java
@@ -4,15 +4,15 @@ import com.codeborne.selenide.Configuration;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 @CucumberOptions(features = "src/test/java/hw6/", glue = {"hw6/pageObjects","hw6/entities"})
 public class CucumberTestngRunnerHw6 extends AbstractTestNGCucumberTests {
 
-    @BeforeMethod(alwaysRun = true)
-    public void beforeMethod() {
+    @BeforeSuite(alwaysRun = true)
+    public void beforeSuite() {
         Configuration.browser = "chrome";
     }
 
